6337b63f51864c959ecd7ae7a5f03406425d3a48
ogt-git <rudxor4@gmail.com>
2025-11-11 17:03:39 +0900

    feat : 새 프로젝트 추가

---
+++ src/main/java/com/example/SecondMarket/SecondMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.SecondMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SecondMarket/SecondMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecondMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
